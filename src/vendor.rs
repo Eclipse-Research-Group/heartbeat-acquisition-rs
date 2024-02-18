983b0d7f13c02f2028372806aae8f10e1a4f5bce
Maris Usis <mau13@case.edu>
2024-02-18 21:56:16 +0000

    Fix vendor pin control

--- src/vendor.rs
+++ src/vendor.rs
@@ -5,14 +5,15 @@ pub fn setup_pins() -> Result<()> {
     use rppal::gpio::{Gpio, OutputPin};
     use std::error::Error;
 
-
     // Just in case, set raspberry pi pins to input
     let gpio = Gpio::new()?;
-    let pin_gps_fix_led = gpio.get(26)?.into_input_pullup();
-    let pin_gps_fix = gpio.get(18)?.into_input_pullup();
-    let pin_gps_fix = gpio.get(4)?.into_input_pullup();
-    let pin_gps_fix = gpio.get(17)?.into_input_pullup();
-    let pin_gps_fix = gpio.get(16)?.into_input_pullup();
+    let _pin_gps_fix_led = gpio.get(26)?.into_input();
+    let _pin_gps_fix = gpio.get(18)?.into_input();
+    let _pin_pps_1 = gpio.get(4)?.into_input();
+    let _pin_pps_2 = gpio.get(17)?.into_input();
+    let _pin_overload = gpio.get(16)?.into_input();
+    let _pin_gps_rx = gpio.get(13)?.into_input();
+    let _pin_gps_tx = gpio.get(12)?.into_input();
 
     Ok(())
 }
